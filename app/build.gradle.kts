1060c2c4ac38de75933c803f558a7c5716ed8624
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-01-18 22:39:26 +0100

    add interceptor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.runtime.livedata)
-
+    implementation(libs.logging.interceptor)
     implementation(libs.firebase.auth)
 /*    implementation(libs.firebase.kt)*/
 /*    implementation(libs.androidx.recyclerview)*/
@@ -87,6 +87,7 @@ dependencies {
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.junit.junit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
