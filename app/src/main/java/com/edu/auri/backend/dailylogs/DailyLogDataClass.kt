f89ebfdbcf40a775de490ecb3ae8f5863e16b4c6
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-01-20 00:52:55 +0100

    add KDoc to DailyLogDataClass.kt, LogDailyActivity.kt

--- app/src/main/java/com/edu/auri/backend/dailylogs/DailyLogDataClass.kt
+++ app/src/main/java/com/edu/auri/backend/dailylogs/DailyLogDataClass.kt
@@ -1,6 +1,29 @@
 package com.edu.auri.backend.dailylogs
+
 import com.google.firebase.Timestamp
 
+/**
+ * Data class representing a daily log entry containing various health and lifestyle metrics.
+ *
+ * Each instance of [DailyLogDataClass] encapsulates information recorded in a single day, such as
+ * consumption of substances, sleep hours, social interactions, and physical activities.
+ *
+ * @property alcohol The recorded amount or score related to alcohol consumption.
+ * @property angerLevel The recorded level of anger experienced during the day.
+ * @property anxietyLevel The recorded level of anxiety experienced during the day.
+ * @property cigarettes The number of cigarettes smoked during the day.
+ * @property cupsOfCoffee The number of cups of coffee consumed during the day.
+ * @property drugs A Boolean indicating whether drugs were used on the day.
+ * @property gratification The recorded level of gratification or satisfaction experienced.
+ * @property litersOfWater The number of liters of water consumed during the day.
+ * @property mood A textual description of the overall mood.
+ * @property sleepHours The number of hours slept during the day.
+ * @property socialInteractions The count or measure of social interactions during the day.
+ * @property stressLevel The recorded level of stress experienced during the day.
+ * @property sweets The number of sweets consumed during the day.
+ * @property timestamp The [Timestamp] indicating when the log was recorded.
+ * @property workoutTime The duration of the workout in minutes.
+ */
 data class DailyLogDataClass(
     val alcohol: Int? = null,
     val angerLevel: Int? = null,
@@ -18,5 +41,3 @@ data class DailyLogDataClass(
     val timestamp: Timestamp? = null,
     val workoutTime: Int? = null
 )
-
-
