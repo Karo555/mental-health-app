01dcc7ed5dd901aa1d37ff151afbdad67c4f34ae
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-01-20 15:55:03 +0100

    commit pre pull

--- app/src/main/java/com/edu/auri/frontend/welcomescreen/Welcome Screen.kt
+++ app/src/main/java/com/edu/auri/frontend/welcomescreen/Welcome Screen.kt
@@ -1,14 +1,11 @@
 package com.edu.auri.frontend.welcomescreen
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -22,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
