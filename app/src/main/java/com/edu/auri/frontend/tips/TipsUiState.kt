1c1c67bfe0f960c8407fbcc53be12093310b1923
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-01-20 01:02:08 +0100

    delete depreciated files

--- app/src/main/java/com/edu/auri/frontend/tips/TipsUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.edu.auri.frontend.tips
-
-sealed class TipsUiState {
-    object Idle : TipsUiState()
-    object Loading : TipsUiState()
-    data class Success(val message: String) : TipsUiState()
-    data class Error(val error: String) : TipsUiState()
-}
