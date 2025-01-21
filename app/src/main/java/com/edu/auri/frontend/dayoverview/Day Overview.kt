c79fca811355887a3c9d87097908909aefa0c934
timGalk <tivron@outlook.com>
2025-01-21 11:41:01 +0100

    added docstrings for day overveiew functions

--- app/src/main/java/com/edu/auri/frontend/dayoverview/Day Overview.kt
+++ app/src/main/java/com/edu/auri/frontend/dayoverview/Day Overview.kt
@@ -5,25 +5,20 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,7 +32,12 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-@OptIn(ExperimentalMaterial3Api::class)
+/**
+ * A composable function that displays the day overview screen.
+ *
+ * @param modifier The modifier to be applied to the composable function.
+ * @param navController The navigation controller to be used for navigation.
+ */
 @Composable
 fun DayOverviewScreen(modifier: Modifier = Modifier, navController: NavController) {
     var mood by remember { mutableStateOf("") }
