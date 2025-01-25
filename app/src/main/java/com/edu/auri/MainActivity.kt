4ae6e5c3967e96155746e778dd4e8758bc1965bf
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-01-25 11:51:14 +0100

    add KDoc to MainActivity.kt

--- app/src/main/java/com/edu/auri/MainActivity.kt
+++ app/src/main/java/com/edu/auri/MainActivity.kt
@@ -15,7 +15,19 @@ import com.edu.auri.backend.registration.AuthViewModel
 import com.edu.auri.frontend.navigation.AuriNavigation
 import com.edu.auri.ui.theme.AuriTheme
 
+/**
+ * Main activity of the Auri application.
+ *
+ * This activity is responsible for setting up the application's UI using Jetpack Compose.
+ * It initializes necessary ViewModels and manages the navigation system.
+ */
 class MainActivity : ComponentActivity() {
+    /**
+     * Called when the activity is starting. Sets up the UI with the app theme and navigation.
+     *
+     * @param savedInstanceState If the activity is re-initialized after being shut down,
+     * this Bundle contains the data it most recently supplied.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val authViewModel: AuthViewModel by viewModels()
